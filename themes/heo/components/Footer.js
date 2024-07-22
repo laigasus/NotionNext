d9b32c4d3bde45912e4acbcf7dcefa254f3618a3
Ok JaeOok <okjaeook98@gmail.com>
2024-07-23 01:40:10 +0900

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -10,19 +10,6 @@ const Footer = ({ title }) => {
 
   return (
     <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
-      {/* 颜色过度区 */}
-      <div
-        id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
-      />
-
-      {/* 社交按钮 */}
-      <div className='w-full h-24'>
-        <SocialButton />
-      </div>
-
-      <br />
-
       {/* 底部页面信息 */}
       <div
         id='footer-bottom'

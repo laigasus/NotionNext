6ab2f3e280b7437eecdef37920345fc664e9186f
Ok JaeOok <okjaeook98@gmail.com>
2024-08-05 17:05:49 +0900

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,10 +2,7 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1 이후 버전에서는 환영 메시지를 blog.config.js에서 설정하며, 여러 메시지를 쉼표 ','로 구분합니다.
   HEXO_HOME_BANNER_GREETINGS: [
-  'Hi, 저는 프로그래머입니다',
-  'Hi, 저는 직장인입니다',
-  'Hi, 저는 식사하는 사람입니다',
-  '제 블로그에 오신 것을 환영합니다🎉'
+  '기록 보관소입니다.'
   ], // 홈페이지 배너 문구
   
   HEXO_HOME_NAV_BUTTONS: true, // 홈페이지에 카테고리 대형 아이콘 버튼 표시 여부
@@ -18,17 +15,17 @@ const CONFIG = {
   HEXO_MENU_INDEX: true, // 홈페이지 표시
   HEXO_MENU_CATEGORY: true, // 카테고리 표시
   HEXO_MENU_TAG: true, // 태그 표시
-  HEXO_MENU_ARCHIVE: true, // 아카이브 표시
-  HEXO_MENU_SEARCH: true, // 검색 표시
+  HEXO_MENU_ARCHIVE: false, // 아카이브 표시
+  HEXO_MENU_SEARCH: false, // 검색 표시
   HEXO_MENU_RANDOM: true, // 랜덤 이동 버튼 표시
   
   HEXO_POST_LIST_COVER: true, // 목록에 글 표지 표시
   HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 목록에서 마우스 오버 시 확대
   
-  HEXO_POST_LIST_COVER_DEFAULT: true, // 표지가 없을 경우 사이트 배경을 기본 표지로 사용
+  HEXO_POST_LIST_COVER_DEFAULT: false, // 표지가 없을 경우 사이트 배경을 기본 표지로 사용
   HEXO_POST_LIST_SUMMARY: true, // 글 요약
   HEXO_POST_LIST_PREVIEW: false, // 글 미리보기
-  HEXO_POST_LIST_IMG_CROSSOVER: true, // 블로그 목록 이미지 좌우 교차
+  HEXO_POST_LIST_IMG_CROSSOVER: false, // 블로그 목록 이미지 좌우 교차
   
   HEXO_ARTICLE_ADJACENT: true, // 이전 글 및 다음 글 추천 표시
   HEXO_ARTICLE_COPYRIGHT: true, // 글 저작권 표시
